141b72b20da93d73c75955a69d724f707792c648
ohjon93 <ohjon93@uw.edu>
2014-10-28 10:14:41 -0700

    changed app.js, added awesomesauce to main.css and imported js to index.html

--- js/app.js
+++ js/app.js
@@ -4,3 +4,51 @@
 
 "use strict";
 
+//select all navigation links
+var allNavlinks = $('nav a');
+
+//select all imgs
+var allImgs = $('img');
+
+//select all sections
+var allSections = $('section');
+
+
+function onReady() {
+	$('nav').slideUp();
+	$('nav').slideDown();
+	allSections.hide();
+	
+	$('#exit-button').dblclick(function() {
+		//show the modal to the world!
+		$('#confirm-exit-modal').modal();		
+	});
+	
+	$('#confirm-exit-button').click(function() {
+		window.location.href="http://courses.washington.edu/info343/morris/";
+	});
+}
+
+allImgs.hover(function() {
+	console.log($(this));
+	$(this).addClass('awesomesauce');
+});
+
+allImgs.mouseout(function() {
+	$(this).removeClass('awesomesauce');
+});
+
+allNavlinks.click(function() {
+	//the cooler jQuery this - it's totally money
+	console.log($(this).attr('href'));
+	
+	//the plain version of this...it's ok.
+	console.log(this);
+	
+	//acts on the jQuery object containing this element
+	//$(this).addClass('awesomesauce');
+	
+	$($(this).attr('href')).fadeToggle(1000).addClass('awesomesauce');
+});
+
+$(onReady);
